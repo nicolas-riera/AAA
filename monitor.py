1ee54a782c770cba409bf37ce30afe221ec899b5
nicolas-riera <nicolas.riera@laplateforme.io>
2025-12-08 13:02:31 +0100

    Added user and cpu core counts

--- monitor.py
+++ monitor.py
@@ -3,12 +3,23 @@ import platform
 import socket
 import os
 
+def get_users():
+    users = []
+    try:
+        with open("/etc/passwd", "r") as f:
+            for line in f:
+                username = line.split(":")[0]
+                users.append(username)
+    except:
+        pass
+    return len(users)
+
 machine_name = socket.gethostname()
 os_name = platform.platform()
 uptime = os.popen('uptime -p').read()[:-1]
-user_nb = None
+user_nb = get_users()
 
-cpu_cores_nb = None
+cpu_cores_nb = os.cpu_count()
 cpu_frequency = None
 cpu_usage = None
 
@@ -26,3 +37,5 @@ txt_file_nb = None
 py_file_nb = None
 pdf_file_nb = None
 jpg_file_nb = None
+
+print(machine_name,os_name,uptime,user_nb,cpu_cores_nb, cpu_frequency,cpu_usage,total_ram,ram_usage_nb,ram_usage_percentage,ip_adress,process1,process2,process3,txt_file_nb,py_file_nb,pdf_file_nb,jpg_file_nb)
